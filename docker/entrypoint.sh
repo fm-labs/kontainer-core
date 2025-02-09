1f1e8f76b87fe8e4384c81ad0c144b8d4bdef1eb
fm-labs <flowmotionlabs@gmail.com>
2025-02-09 19:56:47 +0100

    Added env variables defaults

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -26,22 +26,25 @@ echo "ID: ${CURRENTUSERID} / ${KUSER_ID}"
 
 export PYTHON_UNBUFFERED=1
 export PYTHONPATH=/app/src:$PYTHONPATH
-export AGENT_DATA_DIR=/app/data
-export AGENT_HOST=0.0.0.0
-export AGENT_PORT=5000
+export AGENT_DATA_DIR=${AGENT_DATA_DIR:-/app/data}
+export AGENT_HOST=${AGENT_HOST:-127.0.0.1}
+export AGENT_PORT=${AGENT_PORT:-5000}
 
 # Gunicorn settings
 NAME="kstack-agent-api"
 USER=$(whoami)
 GROUP=$(whoami)
-NUM_WORKERS=3
 WSGI_APP=wsgi:app
-LOG_LEVEL=debug # todo change to info in production
+NUM_WORKERS=${AGENT_WORKERS:-3}
+LOG_LEVEL=${LOG_LEVEL:-info}
 
 case $1 in
 
   "devserver")
     init_services
+
+    # Allow connections from any host
+    # export AGENT_HOST=0.0.0.0
 
     echo "Starting devserver ..."
     exec python3 /app/agent.py
@@ -50,6 +53,9 @@ case $1 in
 
   "gunicorn-tcp")
     init_services
+
+    # Only allow connections from localhost
+    # export AGENT_HOST=127.0.0.1
 
     BIND=0.0.0.0:5000
     echo "Starting gunicorn (${BIND}) ..."
