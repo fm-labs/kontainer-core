6e4b34a4659150a83e83565ae79b78392fb30f3a
fm-labs <flowmotionlabs@gmail.com>
2025-02-21 20:33:51 +0100

    Added templates api

---
+++ src/kstack/agent/server/templates_api.py
@@ -0,0 +1,74 @@
+import flask
+from flask import jsonify, request, url_for
+
+from kstack.agent.admin.templates import find_templates, write_template, download_template, load_template
+
+templates_api_bp = flask.Blueprint('templates_api', __name__, url_prefix='/api/templates')
+
+
+@templates_api_bp.route('', methods=["GET"])
+def list_templates():
+    """
+    Returns a list of available templates.
+    """
+    template_ids = find_templates()
+    templates = [{
+        'template_id': template_id,
+        'label': template_id,
+        'url': url_for('templates_api.get_template', template_id=template_id, _external=True)
+    } for template_id in template_ids]
+    return jsonify(templates)
+
+
+@templates_api_bp.route('', methods=["POST"])
+def add_template():
+    """
+    Adds a new template.
+    """
+    data = request.get_json()
+    template_id = data.get('template_id', None)
+    template_content = data.get('content', None)
+    template_url = data.get('url', None)
+
+    if template_id is None:
+        return jsonify({'error': 'template_id is required'}), 400
+
+    if template_content is None and template_url is None:
+        return jsonify({'error': 'content or url is required'}), 400
+
+    if template_content is not None and template_url is not None:
+        return jsonify({'error': 'content and url are mutually exclusive'}), 400
+
+    if template_content is not None:
+        write_template(template_id, template_content)
+        return jsonify({'template_id': template_id, 'status': 'created'}), 201
+
+    if template_url is not None:
+        download_template(template_id, template_url)
+        return jsonify({'template_id': template_id, 'status': 'created'}), 201
+
+
+@templates_api_bp.route('/<string:template_id>', methods=["GET"])
+def get_template(template_id):
+    """
+    Returns the contents of a specific template.
+    """
+    template_contents = load_template(template_id)
+    if template_contents is None:
+        return jsonify({'error': 'Template not found'}), 404
+
+    # return jsonify({'template_id': template_id, 'content': template_contents})
+    return jsonify(template_contents)
+
+
+@templates_api_bp.route('/<string:template_id>', methods=["POST"])
+def update_template(template_id):
+    """
+    Updates an existing template.
+    Does the same as 'add_template' but checks if the template exists first.
+    """
+    template_contents = load_template(template_id)
+    if template_contents is None:
+        return jsonify({'error': 'Template not found'}), 404
+
+    add_template()
--- src/kstack/agent/srv.py
+++ src/kstack/agent/srv.py
@@ -6,12 +6,13 @@ from .server.engine_api import engine_api_bp
 from .server.container_api import container_api_bp
 from .server.environments_api import environments_api_bp
 from .server.images_api import images_api_bp
-#from .server.kube_namespaces_api import kube_namespaces_api_bp
-#from .server.kube_pods_api import kube_pods_api_bp
 from .server.networks_api import networks_api_bp
 from .server.stacks_api import stacks_api_bp
 from .server.tasks_api import tasks_api_bp
+from .server.templates_api import templates_api_bp
 from .server.volumes_api import volumes_api_bp
+#from .server.kube_namespaces_api import kube_namespaces_api_bp
+#from .server.kube_pods_api import kube_pods_api_bp
 
 from .stacks.stacksmanager import StacksManager
 
@@ -20,7 +21,6 @@ StacksManager.enumerate()
 @app.route('/', methods=["GET"])
 def index():
     status_data = {
-        "version": "1.0.0",
         "status": "OK"
     }
     return jsonify(status_data)
@@ -29,6 +29,7 @@ def index():
 app.register_blueprint(environments_api_bp)
 # app.register_blueprint(system_api_bp)
 app.register_blueprint(tasks_api_bp)
+app.register_blueprint(templates_api_bp)
 
 # Docker API
 app.register_blueprint(engine_api_bp)
