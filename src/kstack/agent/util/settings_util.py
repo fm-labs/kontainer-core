d3616f8ccd3194b044f27ca203f153eabb3a9a89
fm-labs <flowmotionlabs@gmail.com>
2025-03-15 16:10:42 +0100

    Fall back to using the JWT secret in memory, if generated key can not be written

--- src/kstack/agent/util/settings_util.py
+++ src/kstack/agent/util/settings_util.py
@@ -8,9 +8,15 @@ def get_or_create_jwt_secret(secret_file_path: str):
 
     else:
         jwt_secret = os.urandom(32)
-        with open(secret_file_path, 'wb') as f:
-            f.write(jwt_secret)
 
-        print("Wrote new JWT secret to file", secret_file_path)
+        try:
+            with open(secret_file_path, 'wb') as f:
+                f.write(jwt_secret)
+
+            print("Wrote new JWT secret to file", secret_file_path)
+        except Exception as e:
+            #print(e)
+            print("Failed to write JWT secret to file", secret_file_path, repr(e))
+            # Fall back to using the JWT secret in memory
 
     return jwt_secret
