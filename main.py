ec1275a335d4de6fcfb3a4fc406758fc520eb62f
Manuel Staufer <manuel.staufervb@gmail.com>
2026-01-30 08:04:14 +0000

    Poetry replaced with UV and added Ruff and Ty for Type Checking and Code Formatting also changed Imports

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 sys.path.append("./src")
 
-from kontainer import settings
+from kontainer.settings import KONTAINER_HOST, KONTAINER_PORT # TODO: Chech this [mstvb] Replaced with Specific Imports
+from kontainer.settings import KONTAINER_DEBUG, KONTAINER_DATA_DIR # TODO: Check this [mstvb] Replaced with specific Imports
 from kontainer.srv import app # ! Importing from 'srv' module not 'app' module !
 
 # Make sure all tasks are imported, so that Celery can find them
-from kontainer.celery import celery
 from kontainer.admin.tasks import *
 from kontainer.docker.tasks import *
 from kontainer.stacks.tasks import *
@@ -15,11 +15,11 @@ if __name__ == '__main__':
     #host = os.getenv("KONTAINER_HOST", "127.0.0.1")
     #port = int(os.getenv("KONTAINER_PORT", "5000"))
 
-    host = settings.KONTAINER_HOST
-    port = settings.KONTAINER_PORT
-    debug = settings.KONTAINER_DEBUG
+    host = KONTAINER_HOST
+    port = KONTAINER_PORT
+    debug = KONTAINER_DEBUG
 
-    print(f"KONTAINER_DATA_DIR: {settings.KONTAINER_DATA_DIR}")
+    print(f"KONTAINER_DATA_DIR: {KONTAINER_DATA_DIR}")
     #print(f"KONTAINER_DATA_HOME: {settings.KONTAINER_DATA_HOME}")
     print(f"Starting webserver on port {port}")
 
--- src/wsgi.py
+++ src/wsgi.py
@@ -1,16 +1,17 @@
 
 #from kontainer.app import app
 from kontainer.srv import app # ! Importing from 'srv' module not 'app' module !
-from kontainer import settings
 
+from kontainer.settings import KONTAINER_HOST, KONTAINER_PORT # TODO: Check this [mstvb] Replaced with Specific Imports
+from kontainer.settings import KONTAINER_DATA_DIR, DOCKER_HOME # TODO: Check this [mstvb] Replaced with Specific Imports
 
 if __name__ == '__main__':
 
-    host = settings.KONTAINER_HOST
-    port = settings.KONTAINER_PORT
+    host = KONTAINER_HOST
+    port = KONTAINER_PORT
 
-    print(f"KONTAINER_DATA_DIR: {settings.KONTAINER_DATA_DIR}")
-    print(f"KONTAINER_DATA_HOME: {settings.KONTAINER_DATA_HOME}")
+    print(f"KONTAINER_DATA_DIR: {KONTAINER_DATA_DIR}") 
+    print(f"KONTAINER_DATA_HOME: {DOCKER_HOME}") # TODO: Check This [mstvb] DATA HOME not founded replaced with DOCKER_HOME
     print(f"Starting webserver available at http://{host}:{port}")
 
     app.run(debug=False, port=port, host=host)
